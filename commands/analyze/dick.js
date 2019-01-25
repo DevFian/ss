8d27c00874b522bddd86110626220812a45e55c1
UrangRobot <38397174+UrangRobot@users.noreply.github.com>
2019-01-25 11:29:37 +0700

    Add files via upload

---
+++ commands/analyze/dick.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+const Random = require('random-js');
+
+module.exports = class DickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'dick',
+			aliases: ['dick-size', 'penis', 'penis-size', 'pee-pee', 'pee-pee-size'],
+			group: 'analyze',
+			memberName: 'dick',
+			description: 'Determines your dick size.',
+			nsfw: true,
+			args: [
+				{
+					key: 'user',
+					prompt: 'What user do you want to determine the dick size of?',
+					type: 'user',
+					default: msg => msg.author
+				}
+			]
+		});
+	}
+
+	run(msg, { user }) {
+		const clientAuthor = user.id === this.client.user.id;
+		const random = new Random(Random.engines.mt19937().seed(clientAuthor ? msg.author.id : user.id));
+		const length = random.integer(0, 200);
+		return msg.reply(`8${'='.repeat(clientAuthor ? length + 1 : length)}D`);
+	}
+};
