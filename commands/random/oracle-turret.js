eb45f8ddea5b85df8c9277d0a03040e06174badc
UrangRobot <38397174+UrangRobot@users.noreply.github.com>
2019-01-25 11:34:50 +0700

    Add files via upload

---
+++ commands/random/oracle-turret.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+const quotes = require('../../assets/json/oracle-turret');
+
+module.exports = class OracleTurretCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'oracle-turret',
+			group: 'random',
+			memberName: 'oracle-turret',
+			description: 'Responds with a random Oracle Turret quote.'
+		});
+	}
+
+	run(msg) {
+		return msg.say(quotes[Math.floor(Math.random() * quotes.length)]);
+	}
+};
